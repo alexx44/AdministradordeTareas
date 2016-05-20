708a46811ba01012cac5f210d83706ad31fb6b30
Juan Muñoz <alexx44@gmail.com>
2016-05-20 00:56:00 -0300

    Hola que tal

--- app/MainCtrl.js
+++ app/MainCtrl.js
@@ -4,6 +4,7 @@
     angular.module("AdministradordeTareas").controller("MainCtrl", [MainCtrl]);
     function MainCtrl(scope){
         var vm = this;
+        
         vm.saludo = "Hola que tal"
         
     }
