cf13ebdf122d3ea9ac72f7763e3abb1c74d67b9e
juanshocl <cl.jmunoz@gmail.com>
2016-05-23 16:23:47 -0400

    arreglos

--- app/app.js
+++ app/app.js
@@ -4,6 +4,24 @@
     app.config(["$stateProvider","$urlRouterProvider",
                function($stateProvider, $urlRouterProvider){
                    
+                   $stateProvider
+                   .state("inicio",{
+                          URL:"/",
+                          templateUrl: "paginas/bienvenidos.html"
+                       })
+                   .state("tareas",{
+                          URL:"/tareas",
+                          templateUrl: "paginas/tareas.html"
+                       })
+                   .state("nuevatarea",{
+                          URL:"/nuevatarea",
+                          templateUrl: "paginas/nuevatarea.html"
+                       })
+                   .state("estadisticas",{
+                          URL:"/estadisticas",
+                          templateUrl: "paginas/estadisticas.html"
+                       })
+                   
                    
                }])
     
