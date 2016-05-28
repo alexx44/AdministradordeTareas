ee053d97d82089f0ccf1109e462e68a104124b15
Juan Muñoz <alexx44@gmail.com>
2016-05-28 00:28:15 -0400

    Filter Buscar

--- app/usuarios/usuariosCtrl.js
+++ app/usuarios/usuariosCtrl.js
@@ -9,12 +9,16 @@
     
     function usuariosCtrl(usuariosResource){
         var vm = this;
+        //Obtener usuarios
         var obtenerUsuarios = function(){
             usuariosResource.query(function(data){
                 vm.usuarios = data;
                  });
         }
         obtenerUsuarios();
+        //Buscar Usuarios
+        
+        vm.buscar = "";
         
     }
     
